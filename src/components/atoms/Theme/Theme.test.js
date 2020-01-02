24b7609526760eb07db9a1b8718ec9df2be64154
Alex Wong <alex.wong@clientrelationship.com>
2020-01-02 16:38:07 +0000

    updated theme component test and snapshot

--- src/components/atoms/Theme/Theme.test.js
+++ src/components/atoms/Theme/Theme.test.js
@@ -5,7 +5,7 @@ import Theme from "./Theme"
 import "jest-styled-components"
 
 describe("List Items Testing", () => {
-  const component = shallow(<Theme theme="examplePalette" />)
+  const component = shallow(<Theme />)
   it("Snapshot Testing", () => {
     const tree = toJson(component)
     expect(tree).toMatchSnapshot()
