a20055ed104f09b7adf2cc72b7f107dd9ad73dee
Alex Wong <alex.wong@clientrelationship.com>
2020-01-06 16:57:25 +0000

    changed rowlayout to named import

--- src/stories/components/PropTable.js
+++ src/stories/components/PropTable.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import PropTypes from "prop-types"
 import { colourPalette } from "../../brandColours.ts"
-import RowLayout from "../../index"
+import { RowLayout } from "../../index"
 
 const { primary, secondary, black, error, surface, complimentary } = colourPalette.examplePalette
 
