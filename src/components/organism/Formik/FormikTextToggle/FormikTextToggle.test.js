5aff80365818bf4518ca2b3117f5a09cca6f3baa
Mike  Riddelsdell <miker@theodo.co.uk>
2020-02-18 09:34:29 +0000

    updated snapshots

--- src/components/organism/Formik/FormikTextToggle/FormikTextToggle.test.js
+++ src/components/organism/Formik/FormikTextToggle/FormikTextToggle.test.js
@@ -4,21 +4,41 @@ import FormikTextToggle from "./FormikTextToggle"
 import Form from "../Form"
 
 describe("FormikTextToggle Component Test", () => {
-  const handleChange = jest.fn()
-  const component = mount(
-    <Form initialValues={{ test: true }} handleChange={handleChange}>
-      <FormikTextToggle
-        width="5rem"
-        name="test"
-        label="Test"
-        trueOption="Yes"
-        falseOption="False"
-        id="testId"
-      />
-    </Form>,
-  )
+  const baseProps = {
+    width: "5rem",
+    name: "test",
+    label: "Test",
+    trueOption: "Yes",
+    falseOption: "False",
+    id: "testId",
+  }
+  it("Match last snapshot with base props", () => {
+    const handleChange = jest.fn()
+    const component = mount(
+      <Form initialValues={{ test: true }} handleChange={handleChange}>
+        <FormikTextToggle {...baseProps} />
+      </Form>,
+    )
+    expect(component).toMatchSnapshot()
+  })
 
-  it("Match last snapshot", () => {
+  it("Matches snapshot with classname passed", () => {
+    const handleChange = jest.fn()
+    const component = mount(
+      <Form initialValues={{ test: true }} handleChange={handleChange}>
+        <FormikTextToggle {...baseProps} className="testClassName" />
+      </Form>,
+    )
+    expect(component).toMatchSnapshot()
+  })
+
+  it("Matches snapshot with row = false", () => {
+    const handleChange = jest.fn()
+    const component = mount(
+      <Form initialValues={{ test: true }} handleChange={handleChange}>
+        <FormikTextToggle {...baseProps} row={false} />
+      </Form>,
+    )
     expect(component).toMatchSnapshot()
   })
 })
