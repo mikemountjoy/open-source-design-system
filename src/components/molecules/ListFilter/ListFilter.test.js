ed1f77ae27ff1932abc051b611cc46b83b7df891
Frank Shenton <frankshenton@C02VG2VDHTDD.local>
2020-04-20 17:25:22 +0100

    added unit tests for if the listfilter values are strings, not JSX, and for checking the items have been sorted correctly. Also added snapshot for strings.

--- src/components/molecules/ListFilter/ListFilter.test.js
+++ src/components/molecules/ListFilter/ListFilter.test.js
@@ -6,13 +6,21 @@ import ListFilter from "./ListFilter"
 
 const options = ["Hello", "Foo", "Bar", "World"]
 const formattedOptions = arr => arr.map(item => ({ key: item, value: <p>{item}</p> }))
+const formattedOptionsStrings = arr => arr.map(item => ({ key: item, value: item }))
 
 describe("FilterableList Component Testing", () => {
   const componentFull = mount(<ListFilter items={formattedOptions(options)} />)
+  const componentFullStrings = mount(<ListFilter items={formattedOptionsStrings(options)} />)
   const component = shallow(<ListFilter items={formattedOptions(options)} />)
+  const componentStrings = shallow(<ListFilter items={formattedOptionsStrings(options)} />)
   const emptyComponent = mount(<ListFilter />)
+
   it("should match snapshot", () => {
     const tree = toJson(componentFull)
+    expect(tree).toMatchSnapshot()
+  })
+  it("should match snapshot w/ strings", () => {
+    const tree = toJson(componentFullStrings)
     expect(tree).toMatchSnapshot()
   })
   it("Does not render a list if the options props is empty", () => {
@@ -22,6 +30,9 @@ describe("FilterableList Component Testing", () => {
   it("Render the correct number of items", () => {
     expect(component.find("ListItem").length).toEqual(options.length)
   })
+  it("Render the correct number of items w/ strings", () => {
+    expect(componentStrings.find("ListItem").length).toEqual(options.length)
+  })
   it("Typing in search filters items", () => {
     component.find("Search").simulate("change", {
       currentTarget: {
@@ -30,6 +41,28 @@ describe("FilterableList Component Testing", () => {
     })
     expect(component.find("ListItem").length).toEqual(1)
     expect(component.find("p").length).toEqual(1)
+  })
+  it("Typing in search filters items w/ strings", () => {
+    componentStrings.find("Search").simulate("change", {
+      currentTarget: {
+        value: "Hello",
+      },
+    })
+    const wrapper = componentStrings.find("ListItem")
+    expect(wrapper.get(0).key).toEqual("Hello")
+    expect(wrapper.length).toEqual(1)
+  })
+  it("Search is empty and items are alphabetised w/ strings", () => {
+    componentStrings.find("Search").simulate("change", {
+      currentTarget: {
+        value: "",
+      },
+    })
+    const wrapper = componentStrings.find("ListItem")
+    expect(wrapper.get(0).key).toEqual("Bar")
+    expect(wrapper.get(1).key).toEqual("Foo")
+    expect(wrapper.get(2).key).toEqual("Hello")
+    expect(wrapper.get(3).key).toEqual("World")
   })
   it("Typing bad regex search doesn't break component", () => {
     component.find("Search").simulate("change", {
